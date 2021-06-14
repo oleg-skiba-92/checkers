7370a54418a688c2d07bd8a94dc6fb2fc5497e3c
Oleksandr <e975sasha@gmail.com>
2021-06-14 22:43:16 +0300

    First normal pull ( added button.scss, edited index.scss, added in game 3 buttons)

--- src/client/components/game.svelte
+++ src/client/components/game.svelte
@@ -5,4 +5,7 @@
 
 <div class="fco-game">
   game component
+  <a href="#" class="fco-btn fco-btn--red">first button</a>
+  <div href="#" class="fco-btn fco-btn--red">first button</div>
+  <button href="#" class="fco-btn fco-btn--red">first button</button>
 </div>
