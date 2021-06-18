c14d2c4cf10bd99a8274e14934e953a1c4b5c6e3
Oleksandr <e975sasha@gmail.com>
2021-06-18 10:12:50 +0300

    modified variables.scss, button.scss, game.svelte
    added variables with button color in variables.scss, change some values in button.scss, added button "disabled" in game.svelte

--- src/client/components/game.svelte
+++ src/client/components/game.svelte
@@ -5,7 +5,8 @@
 
 <div class="fco-game">
   game component
-  <a href="#" class="fco-btn fco-btn--red">first button</a>
-  <div href="#" class="fco-btn fco-btn--red">first button</div>
-  <button href="#" class="fco-btn fco-btn--red">first button</button>
+  <a href="#" class="fco-btn fco-btn--red">red button</a>
+  <div href="#" class="fco-btn fco-btn--orange">orange button</div>
+  <button href="#" class="fco-btn fco-btn--green">green button</button>
+  <button href="#" class="fco-btn fco-btn--disabled">disabled button</button>
 </div>
