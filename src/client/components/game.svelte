3192e2364ae9d3ce6f1bc4011dfa63ddf49017a8
Oleksandr <e975sasha@gmail.com>
2021-06-18 11:50:02 +0300

    .fco-btn DONE! really DONE!
    added .fco-btn--full in button.scss, 
    added some buttons in game.svelte to check .fco-btn--full

--- src/client/components/game.svelte
+++ src/client/components/game.svelte
@@ -9,4 +9,9 @@
   <div href="#" class="fco-btn fco-btn--orange">orange button</div>
   <button href="#" class="fco-btn fco-btn--green">green button</button>
   <button href="#" class="fco-btn fco-btn--disabled">disabled button</button>
+
+  <a href="#" class="fco-btn fco-btn--red fco-btn--full">red button full</a>
+  <div href="#" class="fco-btn fco-btn--orange fco-btn--full">orange button full</div>
+  <button href="#" class="fco-btn fco-btn--green fco-btn--full">green button full</button>
+  <button href="#" class="fco-btn fco-btn--disabled fco-btn--full">disabled button full</button>
 </div>
