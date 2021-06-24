3538394941c8ada0ceaf406b1a3118325d271469
Oleg Skiba <oleg.skiba.92@gmail.com>
2021-06-24 16:44:33 +0300

    CONFIGS - pm2

--- src/client/environment.ts
+++ src/client/environment.ts
@@ -1,3 +1,3 @@
 export const PRODUCTION = true;
-export const BASE_SERVER_URL = 'http://localhost:3001';
+export const BASE_SERVER_URL = 'http://localhost:3000';
 // export const BASE_SERVER_URL = 'https://64f9bbd8c71c.ngrok.io';
