cf6bfba2e2715681329250bda1f6cfe5222c2d06
Oleg Skiba <oleg.skiba.92@gmail.com>
2021-07-02 13:10:23 +0300

    CONFIGS - heroku configs

--- src/api/services/core/session.service.ts
+++ src/api/services/core/session.service.ts
@@ -14,7 +14,8 @@ export class SessionService implements IInitializedService {
     let RedisStore = connectRedis(session)
     let redisClient = redis.createClient({
       host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD
     });
     this.redisStore = new RedisStore({
       client: redisClient
