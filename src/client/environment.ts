f05ad359d513d18e8891acc64a7eda737fae47dc
Oleg Skiba <oleg.skiba.92@gmail.com>
2021-07-13 15:38:00 +0300

    CONFIGS - base api url

--- src/client/environment.ts
+++ src/client/environment.ts
@@ -1,3 +1,3 @@
 export const PRODUCTION = true;
-export const BASE_SERVER_URL = 'http://localhost:3000';
+export const BASE_SERVER_URL = 'https://fco-checkers.herokuapp.com/';
 // export const BASE_SERVER_URL = 'https://64f9bbd8c71c.ngrok.io';
