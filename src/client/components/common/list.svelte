ea0797965499bb50edbfbdf5237d73e7a076d2d2
Oleksandr <e975sasha@gmail.com>
2021-07-14 22:43:05 +0300

    Added avatar logic in lists

--- src/client/components/common/list.svelte
+++ src/client/components/common/list.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { IUserInfo } from '../../../models';
-
+  
   export let list: IUserInfo[]; // TODO interface
   export let title: string;
   // TODO btnClasses to btnTypeEnum
@@ -16,13 +16,16 @@
 
   {#each list as item}
     <div class="fco-list__item">
-      
+      {#if item.picture}
         <img src="{item.picture}" alt="avatar">
+      {:else}
+        <img src="../../../assets/avatar.png" alt="avatar">
+      {/if}
 
-        <div class="fco-list__title-info-wrapper">
-          <div class="fco-list__item-title">{item.userName}</div>
-          <div class="fco-list__item-info">{item.rating}</div>
-        </div>
+      <div class="fco-list__title-info-wrapper">
+        <div class="fco-list__item-title">{item.userName}</div>
+        <div class="fco-list__item-info">{item.rating}</div>
+      </div>
         
       {#if actions.length}
         <div class="fco-list__item-actions">
