ff59f83e859f32ffab7dae593d89da912ae926aa
Oleg Skiba <oleg.skiba.92@gmail.com>
2021-08-06 17:07:39 +0300

    CLIENT - notify component

---
+++ src/client/components/common/notify.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let message = 'test message';
+  export let title = 'test title';
+  export let notifyType = 'error';
+</script>
+
+<!--------------------------------HTML CODE-------------------------------->
+
+<div class="fco-notify fco-notify--{notifyType}">
+  <div class="fco-notify__icon">
+    <img alt={notifyType} src="assets/{notifyType}.svg">
+  </div>
+
+  <div class="fco-notify__body">
+    <div class="fco-notify__title">{title}</div>
+    <div class="fco-notify__message">{message}</div>
+  </div>
+</div>
