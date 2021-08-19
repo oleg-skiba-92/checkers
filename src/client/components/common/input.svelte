a92fd8f06c247ec4ae5dd76415655acaffd8bb49
Olexandr <e975sasha@gmail.com>
2021-08-19 11:56:33 +0300

    Input, Login - Don't worry this code has a lot of mistakes

--- src/client/components/common/input.svelte
+++ src/client/components/common/input.svelte
@@ -1,15 +1,23 @@
-<script lang="ts">
+<script>
 
-  export let inputType = "text";
   export let inputId;
-  export let inputName;
-  export let inputPlaceholder;
+  export let inputName = "";
+  export let inputPlaceholder = "";
+  export let inputValue;
   export let labelText;
+  export let errorMessage = "";  
+
+	export let inputType = "text";
+	function typeAction(node) {
+		node.type = inputType;
+	}
   
 </script>
 
 <div class="fco__input-wrapper">
   <label class="fco-label" for={inputId}>{labelText}</label>
   <br>
-  <input class="fco__input-item" type={inputType} id={inputId} name={inputName} placeholder={inputPlaceholder}>
+  <input class="fco__input-item" use:typeAction bind:value={inputValue} id={inputId} name={inputName} placeholder={inputPlaceholder}>
+  <br>
+  <label class="fco-label fco-label--error-message" for={inputId}>{errorMessage}</label>
 </div>
--- src/client/components/login.svelte
+++ src/client/components/login.svelte
@@ -1,6 +1,31 @@
 <script lang="ts">
-  import Input from './common/input.svelte';
 
+  import Input from './common/input.svelte';
+  //import {EApiValidationError} from '../../models' // Бред
+  import { usersService } from '../services';
+  
+  let inputValue = {
+    password: '',
+    email: '',
+  };
+  
+  /*// Якийсь бред
+  async function catchLoginError() {
+		const loginDate = await fetch(EApiValidationError);
+		
+		if (EApiValidationError) {
+			return "";
+		} else {
+			throw new Error('Error message!!!');
+		}
+	}
+*/
+   // при натисненні на кнопку login відправляються дані
+   const loginToGame = () => {
+    usersService.login(inputValue);
+  }
+  //let promise = loginToGame();
+  
 </script>
 
 <!--------------------------------HTML CODE-------------------------------->
@@ -8,14 +33,26 @@
 <div class="fco-login">
   
   <h2 class="fco-login__title">Login</h2>
-  <form class="fco-login-form" action="https://echo.htmlacademy.ru" method="POST">
-    <Input labelText="Login" inputType="email" inputName="login" inputId="user-login" inputPlaceholder="example@mail.com"/>
-  
-    <Input labelText="Password" inputType="password"  inputName="password" inputId="user-password" inputPlaceholder="******"/>
+  <form class="fco-login-form">
+    
+    <!-- 
+      ще одна тупость
+    {#await promise}
+    <p>хз шо це...</p>
+    {:then 'Error Message!!!'}
+    <Input bind:inputValue={inputValue.email} labelText="Login" errorMessage="" inputType="text" inputName="login" inputId="user-login" inputPlaceholder="example@mail.com"/>
+    {:catch error}
+    <Input bind:inputValue={inputValue.email} labelText="Login" errorMessage="123" inputType="text" inputName="login" inputId="user-login" inputPlaceholder="example@mail.com"/>
+    {/await}
+    -->
+
+    <Input bind:inputValue={inputValue.email} labelText="Login" errorMessage="" inputType="text" inputName="login" inputId="user-login" inputPlaceholder="example@mail.com"/>
+
+    <Input bind:inputValue={inputValue.password} labelText="Password" inputType="password" inputName="password" inputId="user-password" inputPlaceholder="******"/>
     
     <!-- Button login Registration -->
     <div class="fco-login__button-wrapper">
-      <button class="fco-btn fco-btn--orange fco-btn--align-center fco-btn--width">Login</button>
+      <button type="button" class="fco-btn fco-btn--orange fco-btn--align-center fco-btn--width" on:click={()=> loginToGame()}>Login</button>
       <button class="fco-btn fco-btn--orange fco-btn--align-center fco-btn--width">Registration</button>
     </div>
   </form>
