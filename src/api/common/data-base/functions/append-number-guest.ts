1a668a316e03783589d5ee1ce3fe06350a442a3b
Oleg Skiba <oleg.skiba.92@gmail.com>
2021-08-30 11:27:19 +0300

    SERVER (rework db) - small fixes

--- src/api/common/data-base/functions/append-number-guest.ts
+++ src/api/common/data-base/functions/append-number-guest.ts
@@ -10,17 +10,19 @@ $$
 
   DECLARE count_guest INTEGER;
   BEGIN
-    SELECT * INTO count_guest FROM ${EDataEntity.GuestCount} LIMIT 1;
-
-    IF count_guest IS NULL THEN 
-      count_guest = 1;
-      INSERT INTO ${EDataEntity.GuestCount} (${EGuestsColumns.Count}) VALUES (1);
-    ELSE
-      count_guest = count_guest + 1;
-      UPDATE ${EDataEntity.GuestCount} SET ${EGuestsColumns.Count} = count_guest;
+    IF NEW.is_guest IS TRUE THEN
+      SELECT * INTO count_guest FROM ${EDataEntity.GuestCount} LIMIT 1;
+  
+      IF count_guest IS NULL THEN 
+        count_guest = 1;
+        INSERT INTO ${EDataEntity.GuestCount} (${EGuestsColumns.Count}) VALUES (1);
+      ELSE
+        count_guest = count_guest + 1;
+        UPDATE ${EDataEntity.GuestCount} SET ${EGuestsColumns.Count} = count_guest;
+      END IF;
+      
+      NEW.${EUsersColumns.UserName} = CONCAT(NEW.${EUsersColumns.UserName}, ' ', count_guest);
     END IF;
-    
-    NEW.${EUsersColumns.UserName} = CONCAT(NEW.${EUsersColumns.UserName}, ' ', count_guest);
     
     RETURN NEW;
   END;
--- src/api/common/data-base/tabels/game-history.db.ts
+++ src/api/common/data-base/tabels/game-history.db.ts
@@ -14,7 +14,7 @@ export const GAME_HISTORY_TBL = {
     {name: EGameHistoryColumns.ID, type: 'VARCHAR(36)', keys: ['NOT NULL', 'UNIQUE', 'PRIMARY KEY']},
     {name: EGameHistoryColumns.History, type: 'VARCHAR(1000)', keys: []},
     {name: EGameHistoryColumns.DateCreated, type: 'VARCHAR(50)', keys: ['DEFAULT NOW()']},
-    {name: EGameHistoryColumns.Time, type: 'NUMBER', keys: ['DEFAULT 0']},
-    {name: EGameHistoryColumns.EndType, type: 'NUMBER', keys: []},
+    {name: EGameHistoryColumns.Time, type: 'INTEGER', keys: ['DEFAULT 0']},
+    {name: EGameHistoryColumns.EndType, type: 'INTEGER', keys: []},
   ]
 };
